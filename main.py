7dfd8ea473f3b53ec35f4e788f5c1ee1a57a028c
Drew Hund <languageinvestigator@gmail.com>
2026-05-23 13:55:00 -0400

    form validations

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, Length
 from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
@@ -11,8 +14,9 @@ bootstrap = Bootstrap5(app)
 app.secret_key = os.getenv("SECRET_KEY")
 
 class LoginForm(FlaskForm):
-    email = StringField('email')
-    password = StringField('password')
+    email = EmailField(label='email', validators=[DataRequired(), Email(message="Please enter a valid email address.")])
+    password = PasswordField(label='password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
 
 
 @app.route("/")
@@ -20,9 +24,19 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html", form=LoginForm())
+    login_form = LoginForm()
+    if request.method == "POST":
+        if login_form.validate_on_submit():
+            if login_form.email.data == os.getenv("SECRET_EMAIL") and login_form.password.data == os.getenv("SECRET_PASSWORD"):
+                return render_template("success.html")
+            else:
+                return render_template("denied.html")
+        else:
+            return render_template("login.html")
+    else:
+        return render_template("login.html", form=login_form)
 
 if __name__ == "__main__":
     app.run(debug=True)
